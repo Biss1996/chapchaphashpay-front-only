d4d6c15f77df5fd5823e9a2fde87c6bca29946ce
Bismark <bettkipkoech45@gmail.com>
2026-04-27 20:24:17 +0300

    pamenty

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-// Example STK Push call
+// STK Push
 export const initiateSTKPush = async (phone, amount) => {
   try {
     const res = await API.post("/stkpush", {
