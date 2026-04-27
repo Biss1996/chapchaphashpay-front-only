73f685ec34b565fb1212dd87a2ee4c60894f1cef
Bismark <bettkipkoech45@gmail.com>
2026-04-27 18:24:24 +0300

    stkpush

--- frontend/src/services/hashpay.js
+++ frontend/src/services/hashpay.js
@@ -4,7 +4,7 @@ import api from "../api";
  * Initiates STK Push via backend (Flask)
  */
 export const initiateSTKPush = async (phone, amount, reference) => {
-  const res = await api.post("/pay", {
+  const res = await api.post("/stkpush", {
     phone,
     amount,
     reference,
